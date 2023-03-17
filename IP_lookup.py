e7cb50536cf01968fab01c566a70de71486b7281
S0akedInBleach <46869654+S0akedInBleach@users.noreply.github.com>
2023-03-16 21:51:39 -0400

    Update IP_lookup.py

--- IP_lookup.py
+++ IP_lookup.py
@@ -1,8 +1,11 @@
+#for linux/chrome
+
 #Capture an IP from a user as a variable in a GUI as ip_addr
 
 import tkinter as tk
 from tkinter import simpledialog
 import webbrowser
+import subprocess
 
 #Capture an IP from a user as a variable in a GUI as ip_addr
 root = tk.Tk()
@@ -14,20 +17,29 @@ print(ip_addr)
 #insert ip_addr into urls
 
 urls = [
-    f"https://www.abuseipdb.com/check/{ip_addr}"
-    f"https://www.virustotal.com/gui/ip-address/{ip_addr}/details"
-    f"https://exchange.xforce.ibmcloud.com/ip/{ip_addr}"
-    f"https://www.shodan.io/host/{ip_addr}"
-    f"https://otx.alienvault.com/indicator/ip/{ip_addr}"
-    f"https://www.threatminer.org/host.php?q={ip_addr}"
-    f"https://www.talosintelligence.com/reputation_center/lookup?search={ip_addr}"
-    f"https://viz.greynoise.io/query/?gnql={ip_addr}"
-    f"https://www.criminalip.io/en/asset/report/{ip_addr}"
-    f"https://www.blocklist.de/en/view.html?ip={ip_addr}"
+    f"https://www.abuseipdb.com/check/{ip_addr}",
+    f"https://www.virustotal.com/gui/ip-address/{ip_addr}/details",
+    f"https://exchange.xforce.ibmcloud.com/ip/{ip_addr}",
+    f"https://www.shodan.io/host/{ip_addr}",
+    f"https://otx.alienvault.com/indicator/ip/{ip_addr}",
+    f"https://www.threatminer.org/host.php?q={ip_addr}",
+    f"https://www.talosintelligence.com/reputation_center/lookup?search={ip_addr}",
+    f"https://viz.greynoise.io/query/?gnql={ip_addr}",
+    f"https://www.criminalip.io/en/asset/report/{ip_addr}",
+    f"https://www.blocklist.de/en/view.html?ip={ip_addr}",
     f"https://www.google.com/search?q=ip:{ip_addr}"
 ]
 
-#open urls in new Edge window
-for url in urls:
-    webbrowser.open_new_tab(url)
-    
+# Construct the  chrome command to open the URLs in a new window in linux
+
+chrome_command = ["google-chrome", "--new-window"] + urls
+
+# Use subprocess to execute the chrome command
+
+subprocess.run(chrome_command)
+
+
+
+
+
+
